0e4e867bf8395439b1a57f0c85a62666389b570f
Juaco97 <jiterreros@uc.cl>
2021-05-02 13:58:31 -0400

    Correccion de Settings

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ import os
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+TAREA_URL="https://iic3103-t2-joaquinterreros.herokuapp.com"
  #Hola
 
 # LOCAL
