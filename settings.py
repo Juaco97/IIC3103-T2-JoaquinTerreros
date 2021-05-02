0a0925e3fe746fb0567fd28417ae79d022a49a23
Juaco97 <jiterreros@uc.cl>
2021-05-02 00:11:32 -0400

    De prueba en setting

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+ #Hola
 
 # LOCAL
 #DATABASE_URL = 'sqlite:///test.db'
